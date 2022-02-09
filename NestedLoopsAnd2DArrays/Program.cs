349e3437b2172804123393c111bc480912e19448
YBontGymreig <davee@shrewsbury.ac.uk>
2022-02-09 09:20:50 +0000

    Add project files.

---
+++ NestedLoopsAnd2DArrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NestedLoopsAnd2DArrays
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            char[] letters = { 'a', 'b', 'c' };
+            int[] nums = { 1, 2, 3, 4 };
+
+            //write code to print out all the one letter and one number permutations of letters and numbers, e.g. "a1","a2"
+
+            // now write code that saves permutations in a 2d array
+
+            string[,] perms = new string[3, 4];
+
+        }
+    }
+}
